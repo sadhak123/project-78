390caf94a5476965c752147b02eda6d319b5b832
sadhak123 <88040141+sadhak123@users.noreply.github.com>
2021-11-30 21:19:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_name = "sadhak";
+var last_name = "ruhela";
+var a = 10;
+var b = 5;
+var add = a + b ;
+console.log(first_name)
+console.log(last_name)
+console.log(first_name.concat(last_name))
+console.log(a)
+console.log(b)
+console.log(add)
